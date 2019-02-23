e40b79a127fc71e0a508b753c8de43e74cd9c17b
halima <halima97massa@gmail.com>
2019-02-23 09:57:46 +0100

    init spring xml

---
+++ src/main/java/com/halima/developper/ehtp_sig_2019_spring/xml/App.java
@@ -0,0 +1,13 @@
+package com.halima.developper.ehtp_sig_2019_spring.xml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
